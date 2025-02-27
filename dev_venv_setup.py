63e7d1d5487d952805eb8453586237da028564c7
Lucas Henson <lucashenson04@gmail.com>
2025-02-26 16:44:39 -0800

    Setting up a helper script to set up the local virtual environment and dependencies to work with django quickly

---
+++ dev_venv_setup.py
@@ -0,0 +1,20 @@
+# Sets up the python virtual environment required for development!
+
+# Please input the follow line into your command line to run: python dev_venv_setup.py
+
+import os
+import subprocess
+import sys
+
+def create_virtual_environment(env_name="venv"):
+    # Do stuff
+    test = "hi"
+
+def activate_virtual_environment(env_name="venv"):
+    # Do more stuff
+    test="hello even"
+
+if __name__ == "__main__":
+    env_name = input("Enter the name for the virtual environment (Default is 'venv'): ") or "venv"
+    create_virtual_environment(env_name)
+    activate_virtual_environment(env_name)
